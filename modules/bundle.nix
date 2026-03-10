b553885e651669a5bc006c1cab6628234ad43e4f
hollowillow <slavicekmichal@proton.me>
2026-03-10 17:50:40 +0000

    Add bluetooth configuration

--- modules/bundle.nix
+++ modules/bundle.nix
@@ -3,6 +3,7 @@
 	imports = [
 		./wm/hyprland.nix
 		./network/eno.nix
+		./network/bluetooth.nix
 		./system/user.nix
 		./system/pipewire.nix
 	];
@@ -10,4 +11,5 @@
 	# modules.fonts.enable = lib.mkDefault true;
 	modules.system.user.enable = lib.mkDefault true;
 	modules.system.pipewire.enable = lib.mkDefault true;
+	modules.network.bluetooth.enable = lib.mkDefault true;
 }
