e40f8c18a5b36e769290cc4f72070059d02100fb
hollowillow <188023203+hollowillow@users.noreply.github.com>
2026-03-10 18:22:28 +0000

    Update hyprland module

--- modules/wm/hyprland.nix
+++ modules/wm/hyprland.nix
@@ -3,10 +3,26 @@
 	options.modules.wm.hyprland.enable = lib.mkEnableOption "Enables hyprland";
 
 	config = lib.mkIf config.modules.wm.hyprland.enable {
-  		programs.hyprland.enable = true;
+  		programs.hyprland = {
+			enable = true;
+			xwayland.enable = true;
+		};
   		programs.waybar.enable = true;
+		services.getty = {
+			autologinUser = "hollowillow";
+			autologinOnce = true;
+		};
+		environment.loginShellInit = ''
+			[[ "$(tty)" == /dev/tty1 ]] && start-hyprland
+		'';
   		environment.systemPackages = with pkgs; [
 			alacritty
+			waybar-mpris
+			swaybg
+			wl-clipboard
+			dunst # or snc or mako?
+			# ydotool
+			# wev
   		];
 	};
 
