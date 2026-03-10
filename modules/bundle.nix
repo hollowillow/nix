fa342f5a5e6e511a0c25704d072cd048014040ef
hollowillow <slavicekmichal@proton.me>
2026-03-10 18:54:01 +0000

    Add font module

--- modules/bundle.nix
+++ modules/bundle.nix
@@ -6,10 +6,12 @@
 		./network/bluetooth.nix
 		./system/user.nix
 		./system/pipewire.nix
+		./system/fonts.nix
 	];
 
 	# modules.fonts.enable = lib.mkDefault true;
 	modules.system.user.enable = lib.mkDefault true;
 	modules.system.pipewire.enable = lib.mkDefault true;
+	modules.system.fonts.enable = lib.mkDefault true;
 	modules.network.bluetooth.enable = lib.mkDefault true;
 }
