84ae9e34eb3b1949b01cd821162a2f554bad7f1a
hollowillow <slavicekmichal@proton.me>
2026-03-10 15:10:39 +0000

    Modularise user config

--- modules/bundle.nix
+++ modules/bundle.nix
@@ -3,7 +3,9 @@
 	imports = [
 		./wm/hyprland.nix
 		./network/eno.nix
+		./system/user.nix
 	];
 
 	# modules.fonts.enable = lib.mkDefault true;
+	modules.system.user.enable = lib.mkDefault true;
 }
---
+++ modules/system/user.nix
@@ -0,0 +1,16 @@
+{ pkgs, lib, config, ... }: {
+
+	options.modules.system.user.enable = lib.mkEnableOption "Enables default user config";
+
+	config = lib.mkIf config.modules.system.user.enable {
+  		users.users.hollowillow = {
+    			isNormalUser = true;
+    			extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    			# packages = with pkgs; [
+    			#   tree
+    			# ];
+  		};
+	};
+
+}
+
