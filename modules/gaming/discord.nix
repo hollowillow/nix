2f048b4775c95232124289a53ab95ec5f1c0617e
hollowillow <188023203+hollowillow@users.noreply.github.com>
2026-03-12 00:15:02 +0100

    Fix xdg desktop portal config

--- modules/gaming/discord.nix
+++ modules/gaming/discord.nix
@@ -5,10 +5,18 @@
         config = lib.mkIf config.modules.gaming.discord.enable {
                 environment.systemPackages = with pkgs; [
                         discord-canary
-                        xdg-desktop-portal-hyprland
                         grim
                         slurp
                 ];
+                xdg.portal = {
+                        enable = true;
+                        extraPortals = with pkgs; [
+                                xdg-desktop-portal-hyprland
+                                xdg-desktop-portal-gtk
+                        ];
+                        config.common.default = [ "gtk" ];
+                        config.hyprland.default = [ "gtk" "hyprland" ];
+                };
         };
 
 }
