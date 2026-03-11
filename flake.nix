d7b732771ff8a9dcb37dfc19bb2ae2e978c062be
hollowillow <slavicekmichal@proton.me>
2026-03-11 16:00:21 +0100

    Add minecraft module

--- flake.nix
+++ flake.nix
@@ -2,18 +2,23 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        prismlauncher = {
+                url = "github:diegiwg/prismlauncher-cracked";
+                inputs.nixpkgs.follows = "nixpkgs";
+        };
   };
 
   outputs = { nixpkgs, ... } @ inputs:
   let
-	pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-  	nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-    		modules = [
-		./hosts/nixos/configuration.nix
-		./modules/bundle.nix
-		];
-    	};
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+                specialArgs = { inherit inputs; };
+                modules = [
+                ./hosts/nixos/configuration.nix
+                ./modules/bundle.nix
+                ];
+        };
   };
 }
