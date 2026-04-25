217c197b529841ab7e5c7f28e34a811a2d91b7a7
hollowillow <188023203+hollowillow@users.noreply.github.com>
2026-04-25 21:30:55 +0200

    Update hyprland module

--- modules/wm/hyprland.nix
+++ modules/wm/hyprland.nix
@@ -1,41 +1,44 @@
 { pkgs, lib, config, ... }: {
 
-	options.modules.wm.hyprland.enable = lib.mkEnableOption "Enables hyprland";
+    options.modules.wm.hyprland.enable = lib.mkEnableOption "Enables hyprland";
 
-	config = lib.mkIf config.modules.wm.hyprland.enable {
-  		programs.hyprland = {
-			enable = true;
-			xwayland.enable = true;
-		};
-		services.getty = {
-			autologinUser = "hollowillow";
-			autologinOnce = true;
-		};
-		environment.loginShellInit = ''
-                        if [ -z "$DISPLAY" ] && [ "$(tty)" = /dev/tty1 ]; then
-                                sleep 3; exec start-hyprland
-                        fi
-		'';
-                xdg.portal = {
-                        enable = true;
-                        extraPortals = with pkgs; [
-                                xdg-desktop-portal-hyprland
-                                xdg-desktop-portal-gtk
-                        ];
-                        config.hyprland.default = [ "gtk" "hyprland" ];
-                };
-  		environment.systemPackages = with pkgs; [
-                        grim # xdg dp dependancy
-                        slurp # xdg dp dependancy
-			# alacritty
-                        ghostty
-			swaybg
-			wl-clipboard
-			dunst # or snc or mako?
-			libnotify
-			# ydotool
-			# wev
-  		];
-	};
+    config = lib.mkIf config.modules.wm.hyprland.enable {
+        programs.hyprland = {
+            enable = true;
+            xwayland.enable = true;
+        };
+        services.getty = {
+            autologinUser = "hollowillow";
+            autologinOnce = true;
+        };
+        environment.loginShellInit = ''
+            if [ -z "$DISPLAY" ] && [ "$(tty)" = /dev/tty1 ]; then
+                sleep 3; exec start-hyprland
+            fi
+        '';
+        xdg.portal = {
+            enable = true;
+            extraPortals = with pkgs; [
+                xdg-desktop-portal-hyprland
+                xdg-desktop-portal-gtk
+            ];
+            config.hyprland.default = [ "gtk" "hyprland" ];
+        };
+        environment.systemPackages = with pkgs; [
+            grim # xdg dp dependancy
+            slurp # xdg dp dependancy
+            # alacritty
+            ghostty
+            swaybg
+            wl-clipboard
+            dunst # or snc or mako?
+            libnotify
+            hyprsunset
+            hyprlock
+            hypridle
+            # ydotool
+            # wev
+        ];
+    };
 
 }
