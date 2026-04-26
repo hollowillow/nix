57694d80b67ee967055b7b16441f34608e5fde7d
hollowillow <188023203+hollowillow@users.noreply.github.com>
2026-04-26 11:41:07 +0200

    Update hardware config

--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -9,7 +9,7 @@
   boot = {
         initrd = {
                 availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-                kernelModules = [ ];
+                kernelModules = [ "amdgpu" ];
                 verbose = false;
                 systemd.enable = true;
         };
