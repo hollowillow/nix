2ee55bb0e4cbbe927c7839fcaee075f14b50969b
hollowillow <188023203+hollowillow@users.noreply.github.com>
2026-05-20 21:31:02 +0200

    Add home manager

--- flake.nix
+++ flake.nix
@@ -3,13 +3,17 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
         prismlauncher = {
             url = "github:diegiwg/prismlauncher-cracked";
             # inputs.nixpkgs.follows = "nixpkgs";
         };
     };
 
-    outputs = { nixpkgs, ... } @ inputs:
+    outputs = { nixpkgs, home-manager, ... } @ inputs:
     let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
@@ -18,6 +22,14 @@
             modules = [
                 ./hosts/nixos/configuration.nix
                 ./modules/bundle.nix
+                home-manager.nixosModules.default
+                {
+                    home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        users.hollowillow = ./home/hollowillow/home.nix;
+                    };
+                }
             ];
         };
     };
