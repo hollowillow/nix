e281e99c5cda61ee697a06a1b282159a93e41741
hollowillow <188023203+hollowillow@users.noreply.github.com>
2026-05-21 17:16:02 +0200

    Reformat flake

--- flake.nix
+++ flake.nix
@@ -1,42 +1,44 @@
 {
-    description = "A very basic flake";
+  description = "A very basic flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        firefox-addons = {
-            url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        prismlauncher = {
-            url = "github:diegiwg/prismlauncher-cracked";
-            # inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    prismlauncher = {
+      url = "github:diegiwg/prismlauncher-cracked";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-    outputs = { nixpkgs, home-manager, ... } @ inputs:
+  outputs =
+    { nixpkgs, home-manager, ... }@inputs:
     let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-    in {
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs; };
-            modules = [
-                ./hosts/nixos/configuration.nix
-                ./modules/bundle.nix
-                home-manager.nixosModules.default
-                {
-                    home-manager = {
-                        extraSpecialArgs = { inherit inputs; };
-                        useGlobalPkgs = true;
-                        useUserPackages = true;
-                        users.hollowillow = ./home/hollowillow/home.nix;
-                    };
-                }
-            ];
-        };
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/nixos/configuration.nix
+          ./modules/bundle.nix
+          home-manager.nixosModules.default
+          {
+            home-manager = {
+              extraSpecialArgs = { inherit inputs; };
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.hollowillow = ./home/hollowillow/home.nix;
+            };
+          }
+        ];
+      };
     };
 }
