d0a7b691708919448e46f6fbf931c1fac273ec3d
hollowillow <188023203+hollowillow@users.noreply.github.com>
2026-05-21 22:27:32 +0200

    Add new abbreviations

--- modules/shell/fishAbbr.nix
+++ modules/shell/fishAbbr.nix
@@ -1,36 +1,39 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, config, ... }:
+{
+  options.modules.shell.fishAbbr.enable = 
+    lib.mkEnableOption "Enables fish abbreviations";
 
-    options.modules.shell.fishAbbr.enable = lib.mkEnableOption "Enables fish abbreviations";
+  config = lib.mkIf config.modules.shell.fishAbbr.enable {
+    programs.fish.interactiveShellInit = "bind \" \" expand-abbr or self-insert";
+    programs.fish.shellAbbrs = {
+      # NVIM
+      vi = "nvim ";
+      vim = "nvim ";
 
-    config = lib.mkIf config.modules.shell.fishAbbr.enable {
-        programs.fish.interactiveShellInit = "bind \" \" expand-abbr or self-insert";
-        programs.fish.shellAbbrs = {
-            # NVIM
-            vi = "nvim ";
-            vim = "nvim ";
+      # GIT
+      gs = "git status";
+      ga = "git add ";
+      gc = "git clone ";
+      gcm = "git commit -m \"";
+      gca = "git commit --amend";
+      gcan = "git commit --amend --no-edit";
+      gp = "git push";
+      gpf = "git push --force";
 
-            # GIT
-            gs = "git status";
-            ga = "git add ";
-            gc = "git clone ";
-            gcm = "git commit -m \"";
-            gca = "git commit --amend";
-            gcan = "git commit --amend --no-edit";
-            gp = "git push";
-            gpf = "git push --force";
-
-            # NIX
-            nr = "sudo nixos-rebuild ";
-            nrs = "sudo nixos-rebuild switch";
-            nrb = "sudo nixos-rebuild boot";
-            ns = "nix shell ";
-            nsp = "nix shell nixpkgs#";
-            nsg = "nix shell github:";
-            nd = "nix develop .";
-            nf = "nix flake ";
-            nfi = "nix flake init";
-            nfu = "nix flake update";
-        };
+      # NIX
+      nr = "sudo nixos-rebuild ";
+      nrs = "sudo nixos-rebuild switch";
+      nrb = "sudo nixos-rebuild boot";
+      nrt = "sudo nixos-rebuild test";
+      nrd = "sudo nixos-rebuild dry-run";
+      ns = "nix shell ";
+      nsp = "nix shell nixpkgs#";
+      nsg = "nix shell github:";
+      nd = "nix develop .";
+      nf = "nix flake ";
+      nfi = "nix flake init";
+      nfu = "nix flake update";
     };
+  };
 
 }
